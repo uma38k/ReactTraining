2a6b80e9ab57100bc8f513fea9bdd05201b29059
kumari <uma.a.kumari@capgemini.com>
2025-07-02 15:58:20 +0530

    string changes - testing commit

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ var root = ReactDOM.createRoot(document.getElementById('root'));
 function Main(props) {
     console.log(props)
     return(<div>
-        <h3>Name - {props.fn} {props.ln}</h3>
-        <h3>Message - {props.msg}</h3>
+        <h3>{props.msg}</h3>
+        <h3>{props.fn} {props.ln}</h3>
       </div>)
   };
 
 root.render(<div>
-    <Main msg="Capgemini" fn="Hello" ln="World!!" />
+    <Main msg="Welcome!!" fn="Dan" ln="Brown" />
     <h1>Hello World!</h1>
     <h2>Welcome to React!!</h2>
     <h3>Our First JSX!!</h3>
